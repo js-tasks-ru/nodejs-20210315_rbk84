0b0ecf7824b01c15767f68595d53dd559088e57d
Robert Krepets <robert.krepets@megafon.ru>
2021-03-29 15:53:38 +0500

    Added solution for 5-module 1-task

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,17 +1,29 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
-  const filepath = path.join(__dirname, 'files', pathname);
+  const filePath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      fs.readFile(filePath, (error, data) => {
+        if (error) {
+          res.statusCode = 404;
 
+          if (pathname.includes('/')) {
+            res.statusCode = 400;
+          }
+
+          res.end();
+        } else {
+          res.end(data);
+        }
+      });
       break;
 
     default:
